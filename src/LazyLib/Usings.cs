5260a53d3ba430df024dce534147890cf0527efc
Rafael Souza <rafael.souza1291@gmail.com>
2024-12-27 17:10:52 -0300

    Update Lib

---
+++ src/LazyLib/Usings.cs
@@ -0,0 +1,21 @@
+﻿#region System
+
+global using System.Text;
+global using System.Text.RegularExpressions;
+global using System.Linq.Expressions;
+global using System.Security.Cryptography;
+
+#endregion System
+
+#region LazyLib
+
+global using LazyLib.Enumerators;
+global using LazyLib.Utility;
+
+#endregion LazyLib
+
+#region Nuget
+
+global using Microsoft.EntityFrameworkCore;
+
+#endregion Nuget
